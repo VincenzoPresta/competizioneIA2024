346cbeb376dcdcbf08d6cd5984b6448dbb358081
MatteoGreco <matteoklga@gmail.com>
2024-04-23 18:27:17 +0200

    fix attacking strategy

--- playingStrategies.py
+++ playingStrategies.py
@@ -37,7 +37,8 @@ pawn4line = {
     13: 2,
     14: 1,
 }
-defensive_line = 8
+
+defensive_line = 9
 attacking_line = 8
 last_line = 14
 
@@ -49,30 +50,36 @@ def h_alphabeta_search(game, state, cutoff=cutoff_depth(0)):
     player = state.to_move
 
     @cache1
-    def max_value(state, alpha, beta, depth, action_considerata):
+    def max_value(state, alpha, beta, depth, action_considerata, defensive_line):
         if game.is_terminal(state):
             return game.utility(state, player), None
         if cutoff(game, state, depth):
             return h(state, player, action_considerata, game), None
         v, move = -infinity, None
         for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a), alpha, beta, depth + 1, a)
+            v2, _ = min_value(game.result(state, a), alpha, beta, depth + 1, a, defensive_line)
             if v2 > v:
                 v, move = v2, a
                 alpha = max(alpha, v)
             if v >= beta:
                 return v, move
+            if "exchange" not in move:
+                pawn4line[move[2][0]] += 1
+                pawn4line[move[1][0]] -= 1
+            if pawn4line[defensive_line] < pawn4line[defensive_line + 1]:
+                defensive_line += 1
+                print("defensive line:" + str(defensive_line))
         return v, move
 
     @cache1
-    def min_value(state, alpha, beta, depth,action_considerata):
+    def min_value(state, alpha, beta, depth,action_considerata, defensive_line):
         if game.is_terminal(state):
             return game.utility(state, player), None
         if cutoff(game, state, depth):
             return h(state, player, action_considerata), None
         v, move = +infinity, None
         for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a), alpha, beta, depth + 1, a)
+            v2, _ = max_value(game.result(state, a), alpha, beta, depth + 1, a, defensive_line)
             if v2 < v:
                 v, move = v2, a
                 beta = min(beta, v)
@@ -80,17 +87,20 @@ def h_alphabeta_search(game, state, cutoff=cutoff_depth(0)):
                 return v, move
         return v, move
 
-    return max_value(state, -infinity, +infinity, 0, None)
+    return max_value(state, -infinity, +infinity, 0, None, defensive_line)
 
 
 def h(board, player,action_considerata):
     partenza = action_considerata[1]
     arrivo = action_considerata[2]
     if "capturing" in action_considerata:
-        if arrivo[0] > partenza[0]:
-            return 0.8
-        elif arrivo[0] == partenza[0]:
-            return 0.6
-        else:
-            return 0.4
+        if partenza[0] <= 4:
+            return 0.85
+        if partenza[0] > defensive_line:
+            if arrivo[0] > partenza[0]:
+                return 0.8
+            elif arrivo[0] == partenza[0]:
+                return 0.6
+            else:
+                return 0.4
     return 0 #Se non puo' catturare tutte ugual peso //TODOO
